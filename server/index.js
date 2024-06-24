438b0024694e3adc7ba83fe62d758f2a3694352e
karuppasamyGB <148546076+karuppasamyGB@users.noreply.github.com>
2024-06-24 16:32:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors({
 origin: [https://authentication-eight-coral.vercel.app/],
-methods: ["POST"],
+methods: ["POST","GET"],
 credentials:true
 }));
 
